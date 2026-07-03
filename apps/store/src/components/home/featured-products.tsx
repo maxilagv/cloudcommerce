d8c8c94d59f15a8c8dca9099a46eeca0113856ed
maxilagv <maxilavagetto@gmail.com>
2026-07-02 23:54:36 -0300

    fix(store): restore ESLint lint script

--- apps/store/src/components/home/featured-products.tsx
+++ apps/store/src/components/home/featured-products.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight, ShoppingCart, Star } from "lucide-react";
+import { ShoppingCart, Star } from "lucide-react";
 import { homeFeaturedProducts } from "@/lib/home-data";
 import { formatCOP } from "@/lib/utils";
 import { SectionHeading } from "./section-heading";
